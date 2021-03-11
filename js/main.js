1f1f07eb0aa4481e19d93c60ddffb5fadeee3180
Sebastian <sebastianma2433@gmail.com>
2021-03-11 15:08:35 +0100

    Test

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(e) {   
+    
+    $( "#neon-btn" ).click(function() {
+        /* Header */ 
+
+        $('.header-clean').addClass('header-neon').removeClass('header-clean');
+        
+        /* Banner */
+
+        $('.banner-hero').addClass('hero-neon').removeClass('banner-hero');
+        $('.shape-fill-1').css("fill" , "#000116");
+        
+        /* About */
+
+        $('.about-left').addClass('about-left-neon').removeClass('about-left');
+        $('.about-area').addClass('about-area-neon');
+        $('.span-about').addClass('span-about-neon');
+        $('.about-project').addClass('about-project-neon');
+        $('.about-right').addClass('about-right-neon');
+        
+    });
+    
+ });
